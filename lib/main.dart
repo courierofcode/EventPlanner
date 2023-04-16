2eeb1800557127f08abaf06564d7f59464d1a510
courierofcode <notifyemmanuel@gmail.com>
2023-04-15 21:14:20 -0500

    Launch page options created

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:event_planner/screens/home.dart';
 import 'package:event_planner/screens/signin.dart';
 
-void main() async {
+Future<Widget> selectStartPage() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var email = prefs.getString('email');
+
+  return (email == null) ? const SignInScreen() : const HomeScreen();
+}
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+
+  runApp(MyApp(await selectStartPage()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final Widget startPage;
+  const MyApp(this.startPage, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SignInScreen(),
+      home: startPage,
     );
   }
 }
