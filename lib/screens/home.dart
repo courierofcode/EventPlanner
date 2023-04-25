8297a6f31a77b81bc75cc742470eecd47f4d32d4
courierofcode <notifyemmanuel@gmail.com>
2023-04-25 18:02:22 -0500

    Updated home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,14 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:event_planner/screens/signin.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:event_planner/models/colors.dart';
+import 'package:event_planner/models/events.dart';
+import 'package:event_planner/models/event_adder.dart';
+import 'package:event_planner/models/event_editor.dart';
+import 'package:event_planner/screens/profile.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,22 +18,229 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    const EventPage(),
+    Profile(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              debugPrint("Signed Out");
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SignInScreen()));
-            });
-          },
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month), label: 'Events'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class EventPage extends StatefulWidget {
+  const EventPage({Key? key}) : super(key: key);
+
+  @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+  late DateTime _focusedDay;
+  late DateTime _firstDay;
+  late DateTime _lastDay;
+  late DateTime _selectedDay;
+  late CalendarFormat _calendarFormat;
+  late Map<DateTime, List<Event>> _events;
+  final user = FirebaseAuth.instance.currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusedDay = DateTime.now();
+    _firstDay = DateTime.now().subtract(const Duration(days: 1000));
+    _lastDay = DateTime.now().add(const Duration(days: 1000));
+    _selectedDay = DateTime.now();
+    _calendarFormat = CalendarFormat.month;
+    _events = LinkedHashMap(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    );
+    _loadFirestoreEvents();
+  }
+
+  int getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
+
+  _loadFirestoreEvents() async {
+    final firstDay = DateTime(_focusedDay.year, _focusedDay.month, 1);
+    final lastDay = DateTime(_focusedDay.year, _focusedDay.month + 1, 0);
+
+    _events = {};
+
+    final snap = await FirebaseFirestore.instance
+        .collection('events')
+        .where('user', isEqualTo: FirebaseAuth.instance.currentUser?.uid)
+        .withConverter(
+            fromFirestore: Event.fromFirestore,
+            toFirestore: (event, options) => event.toFirestore())
+        .get();
+
+    for (var doc in snap.docs) {
+      final event = doc.data();
+      final day =
+          DateTime.utc(event.date.year, event.date.month, event.date.day);
+      if (_events[day] == null) {
+        _events[day] = [];
+      }
+      _events[day]!.add(event);
+    }
+    setState(() {});
+  }
+
+  List _getEventsForTheDay(DateTime day) {
+    return _events[day] ?? [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("7645c4"),
+          hexStringToColor("5d8df4"),
+          hexStringToColor("dee6f4")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: ListView(
+          children: [
+            TableCalendar(
+              calendarFormat: _calendarFormat,
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              focusedDay: _focusedDay,
+              firstDay: _firstDay,
+              lastDay: _lastDay,
+              onPageChanged: (focusedDay) {
+                setState(() {
+                  _focusedDay = focusedDay;
+                });
+                _loadFirestoreEvents();
+              },
+              selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
+              onDaySelected: (selectedDay, focusedDay) {
+                print(_events[selectedDay]);
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+              eventLoader: _getEventsForTheDay,
+              calendarStyle: const CalendarStyle(
+                weekendTextStyle: TextStyle(
+                  color: Colors.red,
+                ),
+                selectedDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+              ),
+              calendarBuilders: CalendarBuilders(
+                headerTitleBuilder: (context, day) {
+                  return Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(DateFormat.yMMMMd('en_US').format(day)),
+                  );
+                },
+              ),
+            ),
+            ..._getEventsForTheDay(_selectedDay).map(
+              (event) => EventItem(
+                  event: event,
+                  onTap: () async {
+                    final res = await Navigator.push<bool>(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => EditEvent(
+                            firstDate: _firstDay,
+                            lastDate: _lastDay,
+                            event: event),
+                      ),
+                    );
+                    if (res ?? false) {
+                      _loadFirestoreEvents();
+                    }
+                  },
+                  onDelete: () async {
+                    final delete = await showDialog<bool>(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        title: const Text("Delete Event?"),
+                        content: const Text("Are you sure you want to delete?"),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, false),
+                            style: TextButton.styleFrom(
+                              foregroundColor: Colors.black,
+                            ),
+                            child: const Text("No"),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, true),
+                            style: TextButton.styleFrom(
+                              foregroundColor: Colors.red,
+                            ),
+                            child: const Text("Yes"),
+                          ),
+                        ],
+                      ),
+                    );
+                    if (delete ?? false) {
+                      await FirebaseFirestore.instance
+                          .collection('events')
+                          .doc(event.id)
+                          .delete();
+                      _loadFirestoreEvents();
+                    }
+                  }),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.push<bool>(
+            context,
+            MaterialPageRoute(
+              builder: (_) => AddEvent(
+                firstDate: _firstDay,
+                lastDate: _lastDay,
+                selectedDate: _selectedDay,
+              ),
+            ),
+          );
+          if (result ?? false) {
+            _loadFirestoreEvents();
+          }
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
